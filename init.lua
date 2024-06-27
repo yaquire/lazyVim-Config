5640e252417af393756c2b9b5c39887ae14b5933
Yaq <yaqube147@gmail.com>
2024-06-27 21:56:32 +0800

    Adding the Tailwinds CSS & color

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("lazy").setup({
+  "ap/vim-css-color",
+})
+
+vim.g.css_color_enable = 1
+vim.g.css_color_types = { "css", "scss", "html" }
